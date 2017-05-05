ef21f918230ea2e746b5cbfd628fd0f4ea9d88f9
reactive-firewall <reactive-firewall@users.noreply.github.com>
2017-05-04 18:55:36 -0700

    fix for login CWE-341 case found in testing. fix for posible encrypted password leak under hostile conditions CWE-359 (leading possibly CWE-328). other drive-by-fixes.

--- webroot/PiAP/pages/functions.php
+++ webroot/PiAP/pages/functions.php
@@ -13,10 +13,10 @@ function log_error($some_msg) {
 
 function store_in_session($key,$value)
 {
-	if (isset($_SESSION))
-	{
-		$_SESSION[$key]=strip_input($value);
-	}
+		if (isset($_SESSION))
+		{
+			$_SESSION[$key]=strip_input($value);
+		}
 }
 
 function unset_session($key)
@@ -56,11 +56,24 @@ function http_method()
 
 
 function tool_error_msg($the_error_msg) {
-	return '<div class=\"alert-error\"><div class=\"txt-alert\"><strong>Error</strong>' . strip_input($the_error_msg) . '</div></div>';
+	$temp_error = '<div class=\"alert-error\"><div class=\"txt-alert\"><strong>Error</strong>';;
+	try {
+		$temp_error .= strip_input($the_error_msg);;
+	} catch (Exception $e) {
+		$temp_error = '<div class=\"alert-error\"><div class=\"txt-alert\"><strong>BUG</strong>';;
+		$temp_error .= 'You FOUND A BUG. Please report this to my creator.';;
+	}
+	$temp_error .= '</div></div>';;
+	return $temp_error;
 }
 
 function nativehash($secret, $key) {
-		exec(sprintf('../bin/saltify.py --msg %s --salt %s', escapeshellarg(strip_input($secret)), escapeshellarg(strip_input($key))), $res, $rval);;
+		try {
+			exec(sprintf('../bin/saltify.py --msg %s --salt %s', escapeshellarg(strip_input($secret)), escapeshellarg(strip_input($key))), $res, $rval);;
+		} catch (Exception $e) {
+			header("Location: /pages/error.php?err=Server Refused. (crypt_weak) 406");
+			exit();
+		}
 	if ($rval === 0) {
 		if ($res[0] != "") {
 			return $res[0] ;;
@@ -81,17 +94,17 @@ function sec_session_start() {
 	$httponly = true;
 
 	if (http_method() === false) {
-		header("Location: /pages/error.php?err=Could not initiate a safe session (http_head)");
+		header("Location: /pages/error.php?err=Could not initiate a safe session (http_weak) 406");
 		exit();
 	} else {
 		if (http_method() === 'HEAD') {
-			header("Location: /pages/error.php?err=Could not initiate a safe session (http_head)");
+			header("Location: /pages/error.php?err=Could not initiate a safe session (http_weak) 406");
 			exit();
 		}
 	}
 	// Forces sessions to only use cookies.
 	if (ini_set('session.use_only_cookies', 1) === false) {
-		header("Location: /pages/error.php?err=Could not initiate a safe session (ini_set)");
+		header("Location: /pages/error.php?err=Could not initiate a safe session (crypt_weak) 406");
 		exit();
 	}
 
@@ -110,12 +123,17 @@ function sec_session_start() {
 function get_pepper(){
 	// check the pepper var is empty
 	$pepper = "";;
-	$file = fopen("../files/db/pepper", "r");;
-	while(!feof($file))
-	{
-		$pepper = fgets($file);
-	}
-	fclose($file);;
+	try {
+		$file = fopen("../files/db/pepper", "r");;
+		while(!feof($file))
+		{
+			$pepper = fgets($file);
+		}
+		fclose($file);;
+    } catch (Exception $e) {
+        header("Location: /pages/error.php?err=Security Error. Session not trusted. (crypt_weak) 406");
+		exit();
+    }
 	if ($pepper !== "") {
 		return $pepper;;
 	} else {
@@ -131,7 +149,7 @@ function get_salt($username, $pepper){
 	// XSS protection as we might pass this value
 	$username = scrub_hash($username);
 	$the_user_id = get_user_id($username, $pepper);;
-	exec(sprintf('../../dsauth.py --check --file ../files/db/passwd --user-id %s --pepper %s ;', escapeshellarg($the_user_id), escapeshellarg($pepper)), $res, $rval);
+	exec(sprintf('../../dsauth.py --check --file ../files/db/passwd --user-id %s --pepper %s 2>/dev/null;', escapeshellarg($the_user_id), escapeshellarg($pepper)), $res, $rval);
 	if ($rval === 0) {
 		$salt = $res[0] ;;
 	}
@@ -246,11 +264,11 @@ function checkaffinity() {
 
 function checkallergies($username){
 	if (checkaffinity() === true) {
-		//if (!isset($_SESSION['HTTP_USER_AGENT'])) {
-			//log_error("Banner-Grab attack detected.");;
+		if (!isset($_SERVER['HTTP_USER_AGENT'])) {
+			log_error("Banner-Grab attack detected.");;
 			// Somthing seems off. Time to panic!
-			//return true;
-		//} else {
+			return true;
+		} else {
 			$attempts = get_brutes($username);;
 			if ($attempts < 3) {
 				return false;;
@@ -258,7 +276,7 @@ function checkallergies($username){
 				log_error("Brute force attack detected.");;
 				return true;;
 			}
-		//}
+		}
 	} else {
 		// Somthing seems fake. Time to panic!
 		return true;;
@@ -266,6 +284,8 @@ function checkallergies($username){
 }
 
 function login($username, $password) {
+	// harden timming (if only up to a point) by taking at least 0.5 seconds per attempt
+	$time_to_answer = (microtime(true)+0.5);;
 	//pick the spices
 	$pepper = get_pepper() ;
 	$salt = get_salt($username, $pepper) ;
@@ -273,6 +293,7 @@ function login($username, $password) {
 		// no spice no nice
 		unset($pepper);;
 		unset($salt);;
+		time_sleep_until($time_to_answer);
 		return false ;;
 	} else {
 		// spice is nice
@@ -293,12 +314,13 @@ function login($username, $password) {
 				unset($user_id);;
 				$db_password = '';;
 				unset($db_password);;
+				time_sleep_until($time_to_answer);
 				return false;;
 			} else {
 				// taste test
 				if ($db_password === $password) {
 					// Sweet, it worked
-					$user_browser = scrub_input(get_from_session('HTTP_USER_AGENT'));
+					$user_browser = scrub_input($_SERVER['HTTP_USER_AGENT']);
 					$user_id = scrub_input($user_id);
 					store_in_session('user_id', $user_id);
 					$username = scrub_input($username);
@@ -311,6 +333,7 @@ function login($username, $password) {
 					$password = '';;
 					unset($password);;
 					// all done.
+					time_sleep_until($time_to_answer);
 					return true;
 				} else {
 					// FOOD FIGHT
@@ -324,30 +347,34 @@ function login($username, $password) {
 					unset($password);;
 					$user_id = '';;
 					unset($user_id);;
+					time_sleep_until($time_to_answer);
 					return false;
 				}
 			}
 		} else {
-			// No user exists. 
+			// No user exists.
+			time_sleep_until($time_to_answer);
 			return false;
 		}
 	}
+	time_sleep_until($time_to_answer);
 	return false;
 }
 
 function login_check() {
 	// Check if all session variables are set
 	if (isset($_SESSION['user_id'], $_SESSION['username'], $_SESSION['login_string'])) {
-		//if (!isset($_SESSION['HTTP_USER_AGENT'])){
+		if (!isset($_SERVER['HTTP_USER_AGENT'])){
 			// Somthing seems off. Time to panic!
-		//	return false;
-		//}
+			return false;
+		}
 		$user_id = get_from_session('user_id');
 		$login_string = get_from_session('login_string');
 		$username = get_from_session('username');
-		$user_browser = scrub_input(get_from_session('HTTP_USER_AGENT'));;
+		$user_browser = scrub_input($_SERVER['HTTP_USER_AGENT']);;
 		$pepper = get_pepper() ;;
 		$db_password = get_password($user_id);
+		// I hope they update browsers often enough otherwise CWE-310
 		$login_check = hash('sha512', $db_password . $user_browser);;
 		$db_password = false;
 		unset($db_password);
