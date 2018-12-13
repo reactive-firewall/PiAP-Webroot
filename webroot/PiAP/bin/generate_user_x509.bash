c69d0156eea5ec787d30915e8cfbabd70d7b64ab
reactive-firewall <reactive-firewall@users.noreply.github.com>
2018-12-12 16:56:18 -0800

    fix for ssl TLS stuff part 7

--- webroot/PiAP/bin/generate_user_x509.bash
+++ webroot/PiAP/bin/generate_user_x509.bash
@@ -66,7 +66,7 @@ PATH="/bin:/sbin:/usr/sbin:/usr/bin"
 # CWE-20 if this admin tool is exposed directly
 CN_USERNAME="${1:-operator}"
 # USER_ID - the user id used for the certificate
-USER_ID=$(/srv/dsauth.py -C -f /srv/PiAP/files/db/passwd -X $(sudo -u pocket-www head -n 1 /srv/PiAP/files/db/pepper ) -U "${CN_USERNAME}" | grep -vF "None" 2>/dev/null )
+USER_ID=$("/srv/dsauth.py" -C -f /srv/PiAP/files/db/passwd -X $(sudo -u pocket-www head -n 1 /srv/PiAP/files/db/pepper ) -U "${CN_USERNAME}" | grep -vF "None" 2>/dev/null )
 # Create the Client Key and CSR
 LINK_STUB_PATH="/srv/PiAP/files/db/x509/${USER_ID:-client}"
 FILE_STUB_PATH="/etc/ssl/PiAPCA/certs/${USER_ID:-client}"
